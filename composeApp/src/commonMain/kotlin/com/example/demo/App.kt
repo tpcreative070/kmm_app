c8b35e6c1de936fed38ef147594ea4a4f3dd5d7e
Tran Thanh Phong <>
2025-06-23 12:54:14 +0700

    Adding box and textfield

--- composeApp/src/commonMain/kotlin/com/example/demo/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/demo/App.kt
@@ -2,16 +2,27 @@ package com.example.demo
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -32,6 +43,23 @@ fun App() {
             Button(onClick = { showContent = !showContent }) {
                 Text("Click me!")
             }
+
+            Box(modifier = Modifier.padding(10.dp).border(2.dp, color = Color.Transparent).clip(RoundedCornerShape(20.dp)).background(color = Color.Magenta).padding(10.dp,
+                )) {
+
+                Text("Box", color = Color.White)
+
+            }
+
+            var text by remember { mutableStateOf(("")) }
+            TextField(value = text, onValueChange = {
+                text= it
+            }, placeholder = {
+                Text("Enter name", color = Color.Black)
+            })
+
+
+
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
                 Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
